542552662c668d524e1def37742828073cf3eb56
Saurish Srivastava <saurishs1215@gmail.com>
2022-06-05 18:45:22 -0700

    update(blog): caption

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -117,7 +117,7 @@ const renderBlock = (block) => {
     case 'image':
       const src =
         value.type === 'external' ? value.external.url : value.file.url;
-      const caption = value.caption ? value.caption[0].plain_text : '';
+      const caption = value.caption ? value.caption[0]?.plain_text : '';
       return (
         <figure>
           <img className="mx-auto rounded-md" src={src} alt={caption} />
