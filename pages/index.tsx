0e8ae678321a48939a44fef30b32e93080f2084a
Saurish Srivastava <saurishs1215@gmail.com>
2022-07-03 15:55:30 -0700

    update(connect): typo

--- pages/index.tsx
+++ pages/index.tsx
@@ -186,7 +186,7 @@ export default function Home() {
                 rel="noopener noreferrer"
               >
                 Twitter
-              </a>{' '}
+              </a>
               .
             </p>
           </div>
